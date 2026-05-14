618433f425eb45ab0ddc791de32927868223f0ec
Buddy611 <buddy611@users.noreply.github.com>
2026-05-14 14:34:31 +0200

    Fix libtmux deprecation (active_window instead of attached_window)

--- mcops/config.py
+++ mcops/config.py
@@ -2,7 +2,7 @@ import os
 import sys
 from pathlib import Path
 
-VERSION = "2.0.2"
+VERSION = "2.0.3"
 
 # The base directory where all server data resides.
 # In production, this is usually /opt/mcops.
